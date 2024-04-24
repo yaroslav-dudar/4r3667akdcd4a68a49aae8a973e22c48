5f1a457e624f37c0d24eded75d89a93a00035eda
yaroslav-dudar <flayingfog@gmail.com>
2024-04-24 15:54:27 +0300

    try github secrets

--- secrets.py
+++ secrets.py
@@ -2,4 +2,4 @@ big_secret = "sdgsdkjgnsdkj4ui5i43iu5345ui34iu5iu43u"
 token = {"value": "Bearer sfsafasf4tt34y3"}
 password = "nksdfji347iifb4y3o28ohoohfoh427"
 
-i_am_a_harmless_variable = "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"
+github_access_token = "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"
